4e8a6fe7b17cbd630c7fdf746a1cc41e0e793617
Matan Shavit <matanshavit@users.noreply.github.com>
2021-05-22 14:49:41 -0400

    ue a dark theme using material-ui theme provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import { CssBaseline } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 const App = () => (
-  <>
+  <ThemeProvider theme={darkTheme}>
     <CssBaseline />
     <h1>Camel</h1>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
