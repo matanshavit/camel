78a84429449366f8639f7edc7448405b8075433d
Matan Shavit <matanshavit@users.noreply.github.com>
2021-05-23 20:05:54 -0400

    add page title and add button for page
    button does not do anything yet

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,10 +1,24 @@
-import { Card, CardContent, Typography, makeStyles } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  IconButton,
+  makeStyles,
+} from "@material-ui/core";
+import { Add as AddIcon } from "@material-ui/icons";
 
 import PageLayout from "./PageLayout";
 import PageGrid from "../components/PageGrid";
 import PageGridItem from "../components/PageGridItem";
 
 const useStyles = makeStyles((theme) => ({
+  pageTitleBar: {
+    marginTop: theme.spacing(2),
+  },
+  pageTitle: {
+    marginRight: theme.spacing(1),
+  },
   cardSpacing: {
     marginTop: theme.spacing(1),
   },
@@ -40,8 +54,22 @@ const items: ItemGridCardProps[] = [
 ];
 
 const MainPage = () => {
+  const styles = useStyles();
+
   return (
     <PageLayout>
+      <Grid container alignItems="center" className={styles.pageTitleBar}>
+        <Grid item>
+          <Typography variant="h4" className={styles.pageTitle}>
+            Coffee Drinks
+          </Typography>
+        </Grid>
+        <Grid item>
+          <IconButton aria-label="delete">
+            <AddIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
       <PageGrid spacing={2}>
         {items.map((item) => (
           <ItemGridCard {...item} />
